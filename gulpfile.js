cfc2b6ef0174370c106801965e9faf7b2d8204c3
Anton Krivosheev <krivosheev777@gmail.com>
2020-07-11 18:47:57 +0300

    по главной

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,9 @@ var svgstore = require("gulp-svgstore")
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config.js");
+var webpackStream = require("webpack-stream");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -41,6 +44,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -81,11 +85,16 @@ gulp.task("html", function () {
     .pipe(gulp.dest("build"));
 });
 
+gulp.task("js", function () {
+  return gulp.src("source/js/app.js")
+  .pipe(webpackStream(webpackConfig))
+  .pipe(gulp.dest("build/js"))
+});
+
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
     "source//*.ico"
     ], {
       base: "source"
@@ -97,5 +106,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "js", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
