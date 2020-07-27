0584075e7bd94b24489b230e3e6ce0076154a0ee
Anton Krivosheev <krivosheev777@gmail.com>
2020-07-27 23:54:46 +0300

    пробую делать отзывы

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -154,18 +154,30 @@ ymaps.ready(init);
 function init() {
   var map = new ymaps.Map('map', {
     center: [59.968137, 30.316263],
-    zoom: 9
-  });
-} // var myMap;
-// ymaps.ready(init);
-// function init () {
-//     myMap = new ymaps.Map('map', {
-//         center: [55.76, 37.64], // Москва
-//         zoom: 10
-//     })
-// }
-//поп ап
+    zoom: 15
+  }, {
+    searchControlProvider: 'yandex#search'
+  }); // Создаём макет содержимого.
 
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>');
+  myPlacemarkWithContent = new ymaps.Placemark([59.968137, 30.316263], {
+    hintContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+    balloonContent: 'Санкт-Петербург, набережная реки Карповки, дом 5'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#imageWithContent',
+    // Своё изображение иконки метки.
+    iconImageHref: 'img/icon-location.svg',
+    // Размеры метки.
+    iconImageSize: [32, 40],
+    // Макет содержимого.
+    iconContentLayout: MyIconContentLayout
+  });
+  map.geoObjects.add(myPlacemarkWithContent);
+}
+
+; //поп ап
 
 var rewiewsButton = document.querySelector('.reviews__button');
 var popUpForm = document.querySelector('.pop-up');
--- source/js/app.js
+++ source/js/app.js
@@ -58,13 +58,36 @@ ymaps.ready(init);
 function init () {
     var map = new ymaps.Map('map', {
         center: [59.968137, 30.316263],
-        zoom: 9
-    });
-}
+        zoom: 15
+      }, {
+        searchControlProvider: 'yandex#search'
+      });
 
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        );
+
+        myPlacemarkWithContent = new ymaps.Placemark([59.968137, 30.316263], {
+            hintContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+            balloonContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/icon-location.svg',
+            // Размеры метки.
+            iconImageSize: [32, 40],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    map.geoObjects
+        .add(myPlacemarkWithContent);
+};
 
 //поп ап
-
 let rewiewsButton = document.querySelector('.reviews__button');
 let popUpForm = document.querySelector('.pop-up');
 let buttonClose = document.querySelector('.pop-up__close');
