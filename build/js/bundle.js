3f10eee20c3b2a7347ccd0cda9741a315cd90f3c
Anton Krivosheev <krivosheev777@gmail.com>
2020-08-17 20:54:48 +0300

    пробуем звезды

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -127,20 +127,62 @@ window.addEventListener('DOMContentLoaded', function () {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 var map = function map() {
-  ymaps.ready(init);
-
-  function init() {
-    var map = new ymaps.Map('map', {
+  // ymaps.ready(init);
+  // function init () {
+  //     var map = new ymaps.Map('map', {
+  //         center: [59.968137, 30.316263],
+  //         zoom: 15
+  //       }, {
+  //         searchControlProvider: 'yandex#search'
+  //       });
+  //         // Создаём макет содержимого.
+  //         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+  //             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+  //         );
+  //         myPlacemarkWithContent = new ymaps.Placemark([59.968137, 30.316263], {
+  //             hintContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+  //             balloonContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+  //         }, {
+  //             // Опции.
+  //             // Необходимо указать данный тип макета.
+  //             iconLayout: 'default#imageWithContent',
+  //             // Своё изображение иконки метки.
+  //             iconImageHref: 'img/icon-location.svg',
+  //             // Размеры метки.
+  //             iconImageSize: [32, 40],
+  //             // Макет содержимого.
+  //             iconContentLayout: MyIconContentLayout
+  //         });
+  //   map.geoObjects.add(myPlacemarkWithContent);
+  // };
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
       center: [59.968137, 30.316263],
       zoom: 15
     }, {
       searchControlProvider: 'yandex#search'
-    }); // Создаём макет содержимого.
-
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>');
-    myPlacemarkWithContent = new ymaps.Placemark([59.968137, 30.316263], {
+    }),
+        // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
       hintContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
       balloonContent: 'Санкт-Петербург, набережная реки Карповки, дом 5'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/icon-location.svg',
+      // Размеры метки.
+      iconImageSize: [32, 40],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    }),
+        myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+      hintContent: 'Собственный значок метки с контентом',
+      balloonContent: 'А эта — новогодняя',
+      iconContent: '12'
     }, {
       // Опции.
       // Необходимо указать данный тип макета.
@@ -148,14 +190,17 @@ var map = function map() {
       // Своё изображение иконки метки.
       iconImageHref: 'img/icon-location.svg',
       // Размеры метки.
-      iconImageSize: [32, 40],
+      iconImageSize: [48, 48],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-24, -24],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
+      iconContentOffset: [15, 15],
       // Макет содержимого.
       iconContentLayout: MyIconContentLayout
     });
-    map.geoObjects.add(myPlacemarkWithContent);
-  }
-
-  ;
+    myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
+  });
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (map);
--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -1,36 +1,90 @@
 const map = () => {
-  ymaps.ready(init);
+  // ymaps.ready(init);
 
-  function init () {
-      var map = new ymaps.Map('map', {
-          center: [59.968137, 30.316263],
-          zoom: 15
+  // function init () {
+  //     var map = new ymaps.Map('map', {
+  //         center: [59.968137, 30.316263],
+  //         zoom: 15
+  //       }, {
+  //         searchControlProvider: 'yandex#search'
+  //       });
+
+  //         // Создаём макет содержимого.
+  //         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+  //             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+  //         );
+
+  //         myPlacemarkWithContent = new ymaps.Placemark([59.968137, 30.316263], {
+  //             hintContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+  //             balloonContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+  //         }, {
+  //             // Опции.
+  //             // Необходимо указать данный тип макета.
+  //             iconLayout: 'default#imageWithContent',
+  //             // Своё изображение иконки метки.
+  //             iconImageHref: 'img/icon-location.svg',
+  //             // Размеры метки.
+  //             iconImageSize: [32, 40],
+  //             // Макет содержимого.
+  //             iconContentLayout: MyIconContentLayout
+  //         });
+
+  //   map.geoObjects.add(myPlacemarkWithContent);
+  // };
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.968137, 30.316263],
+            zoom: 15
         }, {
-          searchControlProvider: 'yandex#search'
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+            balloonContent: 'Санкт-Петербург, набережная реки Карповки, дом 5'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/icon-location.svg',
+            // Размеры метки.
+            iconImageSize: [32, 40],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+
+        myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+            hintContent: 'Собственный значок метки с контентом',
+            balloonContent: 'А эта — новогодняя',
+            iconContent: '12'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/icon-location.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
         });
 
-          // Создаём макет содержимого.
-          MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-              '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-          );
-
-          myPlacemarkWithContent = new ymaps.Placemark([59.968137, 30.316263], {
-              hintContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
-              balloonContent: 'Санкт-Петербург, набережная реки Карповки, дом 5',
-          }, {
-              // Опции.
-              // Необходимо указать данный тип макета.
-              iconLayout: 'default#imageWithContent',
-              // Своё изображение иконки метки.
-              iconImageHref: 'img/icon-location.svg',
-              // Размеры метки.
-              iconImageSize: [32, 40],
-              // Макет содержимого.
-              iconContentLayout: MyIconContentLayout
-          });
-
-    map.geoObjects.add(myPlacemarkWithContent);
-  };
+    myMap.geoObjects
+    .add(myPlacemark)
+    .add(myPlacemarkWithContent);
+});
 }
 export default map;
 
